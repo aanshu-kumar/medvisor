9b8ef00f2cdda5564190ff42e6b043d98392b3d6
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2024-03-22 21:22:05 +0530

    Added and tested signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,10 +7,7 @@ const saltRounds = 10;
 
 router.post("/signup", async function (req, res) {
   let success = false;
-  const email = req.body.email;
-  const username = req.body.username;
-  const name = req.body.name;
-  const password = req.body.password;
+  const { username, email, name, password } = req.body;
 
   if (!(username && email && name && password)) {
     return res.status(400).json({
@@ -38,15 +35,55 @@ router.post("/signup", async function (req, res) {
     const data = {
       user: {
         id: user.id,
+        username,
       },
     };
 
-    var authtoken = jwt.sign({ data }, process.env.JWT_SECRET);
+    const authToken = jwt.sign(data, process.env.JWT_SECRET);
     success = true;
-    res.json({ success, authtoken });
+    res.json({ success, authToken, msg: "User created successfully!" });
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Internal server error!");
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  let success = false;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      success = false;
+      return res.status(400).json({
+        success,
+        msg: "Please enter the correct email or create a new account!",
+      });
+    }
+
+    const comparedPassword = await bcrypt.compare(password, user.password);
+    if (!comparedPassword) {
+      success = false;
+      return res
+        .status(400)
+        .json({ success, msg: "Please enter the correct password!" });
+    }
+
+    const data = {
+      user: {
+        id: user.id,
+        username: user.username,
+      },
+    };
+
+    const authToken = jwt.sign(data, process.env.JWT_SECRET);
+    success = true;
+    res.json({ success, authToken, msg: "Login successful!" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      msg: "Internal server error!",
+    });
   }
 });
 
