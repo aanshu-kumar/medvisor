6e42466390208990ed5b83c9f4bd7473607b1992
Anshu Kumar <Anshu.kumar.4125@gmail.com>
2024-04-23 21:58:30 +0530

    Created Banner & Nev

---
+++ client/src/Components/Banner.jsx
@@ -0,0 +1,31 @@
+import { ReactTyped } from "react-typed";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="bg-[#F5EFE6] w-full py-[100px]">
+        <div className="max-w-[1240px] mx-auto text-center my-[80px]">
+          <div className="text-[30px]"> The greatest wealth is health.</div>
+          <div className=" text-[60px] md:text-[80px] p-[20px] font-bold text-[#4F6F52]">
+            Get Fit with us.
+          </div>
+          <div className="text-[40px] md:text-[50px] p-[24px]font-bold text-[#e8d3a2]">
+            Your{" "}
+            <ReactTyped
+              className="p-2 font-bold"
+              strings={[
+                "Personal Health Assistent.",
+                "Gym Partner.",
+                "Yoga Instructor.",
+                "Nutrinist.",
+              ]}
+              typeSpeed={60}
+              loop={true}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Banner;
